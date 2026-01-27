a99b0424150c077290999d00f3ae5943c674e776
farid <abisovff@gmail.com>
2026-01-27 11:26:00 +0400

    ZahraFaridtask

---
+++ src/GitHUb.java
@@ -0,0 +1,19 @@
+import java.util.Scanner ;
+public class GitHUb {
+    public void main() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ad:  ");
+        String Name = sc.nextLine();
+
+        System.out.println("Soyad:  ");
+        String surname = sc.nextLine() ;
+
+        System.out.println("Vezife:  ");
+        String position = sc.nextLine() ;
+
+        System.out.println("Ad : " + Name);
+        System.out.println("Soyad : " + surname);
+        System.out.println("Vezife : " + position);
+    }
+}
